a72a16c194737421ea31a3457bf053d05a541277
Briana Berger <brianamb@fb.com>
2019-06-27 11:24:21 -0700

    milestone 1 - the app now  can click the what day is it button and get the correct day screen

--- app/src/main/java/com/codepath/debuggingchallenges/activities/CurrentDayActivity.java
+++ app/src/main/java/com/codepath/debuggingchallenges/activities/CurrentDayActivity.java
@@ -2,7 +2,9 @@ package com.codepath.debuggingchallenges.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.codepath.debuggingchallenges.R;
 
@@ -17,7 +19,10 @@ public class CurrentDayActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_current_day);
         tvDay = (TextView) findViewById(R.id.tvDay);
-        tvDay.setText(getDayOfMonth());
+        
+        // Before setting text, we must change the int into a string
+        String dayOfMonth = String.valueOf(getDayOfMonth());
+        tvDay.setText(dayOfMonth);
     }
 
     private int getDayOfMonth() {
